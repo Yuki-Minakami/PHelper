c671a223276bf7614757fa22fec8e40d56e19f17
Sue1024 <1589893069@qq.com>
2016-10-25 23:04:15 +0800

    a solution of cpu occupation of setInterval, but not workable

--- getURL/worker1.js
+++ getURL/worker1.js
@@ -8,17 +8,20 @@ console.log('worker1 begin');
 
 var begin =  Number(process.argv[2]);
 var end = Number(process.argv[3]);
-console.log('begin:',begin);
+console.log('begin:',begin,'end:',end);
 var i = begin;
 
-var test = setInterval(function(){
-    console.log(i);
-    if(i>end) {
-        clearInterval(this);
-        process.exit(1);
-    }
+var currentTimeout;
+function fetchURL() {
+  console.log(i);
+  if(i>end) {
+    process.exit(1);
+    clearTimeout(currentTimeout);
+  }
+  else {
+    debugger
     fetch(i,i+10);
     i=i+10;
-},2000);
-
-
+    currentTimeout = setTimeout("fetchURL()", 2000);
+  }
+}
