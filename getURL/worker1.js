53f4e4063f57a3b74633c9990185249019a84f66
likaiboy <1140398276@qq.com>
2016-10-29 11:13:46 +0800

    test

--- getURL/worker1.js
+++ getURL/worker1.js
@@ -10,6 +10,7 @@ var begin =  Number(process.argv[2]);
 var end = Number(process.argv[3]);
 console.log('begin:',begin);
 var i = begin;
+var gcFlag = 0;
 
 var test = setInterval(function(){
     console.log(i);
@@ -19,6 +20,11 @@ var test = setInterval(function(){
     }
     fetch(i,i+10);
     i=i+10;
+   gcFlag++;
+    if(gcFlag>200){
+        global.gc();
+    }
+
 },2000);
 
 
